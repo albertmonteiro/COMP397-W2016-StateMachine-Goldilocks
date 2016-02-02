52e0db996bce8a552725cb66080f577f2becd94e
albertmonteiro <albertmonteiro@live.com>
2016-02-02 17:19:24 -0500

    testing

--- Scripts/config/config.ts
+++ Scripts/config/config.ts
@@ -7,6 +7,18 @@
         public static ONE: number = 1;
         public static TWO: number = 2;
         public static THREE: number = 2;
+        public static FOUR: number = 3;
+        public static FIVE: number = 3;
+        public static SIX: number = 3;
+        public static SEVEN: number = 3;
+        public static EIGHT: number = 3;
+        public static NINE: number = 3;
+        public static TEN: number = 3;
+        public static ELEVEN: number = 3;
+        public static TWELVE: number = 3;
+        public static THIRTEEN: number = 3;
+        public static FOURTEEN: number = 3;
+        public static FIFTEEN: number = 3;
         public static OVER: number = 3;
     }
     
