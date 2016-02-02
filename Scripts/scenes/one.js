3939006ca4b6525c5e73406a4625138b79b1a110
albertmonteiro <albertmonteiro@live.com>
2016-02-02 16:23:59 -0500

    testing right and left button in one.js

--- Scripts/scenes/one.js
+++ Scripts/scenes/one.js
@@ -16,22 +16,22 @@ var scenes;
         // Start Method
         One.prototype.start = function () {
             // add the PLAY label to the scene
-            this._playLabel = new createjs.Text("Albert's scene test 2", "60px Consolas", "#000000");
+            this._playLabel = new createjs.Text("Albert's scene", "60px Consolas", "#000000");
             this._playLabel.regX = this._playLabel.getMeasuredWidth() * 0.5;
             this._playLabel.regY = this._playLabel.getMeasuredHeight() * 0.5;
             this._playLabel.x = config.Screen.CENTER_X;
             this._playLabel.y = config.Screen.CENTER_Y;
             this.addChild(this._playLabel);
-            // add the NEXT button to the PLAY scene
-            this._nextButton = new objects.Button("NextButton", config.Screen.CENTER_X + 100, config.Screen.CENTER_Y + 60);
-            this.addChild(this._nextButton);
-            // NEXT Button event listener
-            this._nextButton.on("click", this._nextButtonClick, this);
-            // add the BACK button to the PLAY scene
-            this._backButton = new objects.Button("BackButton", config.Screen.CENTER_X - 100, config.Screen.CENTER_Y + 60);
-            this.addChild(this._backButton);
-            // BACK Button event listener
-            this._backButton.on("click", this._backButtonClick, this);
+            // add the LEFT button to the PLAY scene
+            this._leftButton = new objects.Button("LeftButton", config.Screen.CENTER_X - 100, config.Screen.CENTER_Y + 60);
+            this.addChild(this._leftButton);
+            // LEFT Button event listener
+            this._leftButton.on("click", this._leftButtonClick, this);
+            // add the RIGHT button to the PLAY scene
+            this._rightButton = new objects.Button("RightButton", config.Screen.CENTER_X + 100, config.Screen.CENTER_Y + 60);
+            this.addChild(this._rightButton);
+            // RIGHT Button event listener
+            this._rightButton.on("click", this._rightButtonClick, this);
             // add this scene to the global stage container
             stage.addChild(this);
         };
@@ -40,13 +40,13 @@ var scenes;
         };
         //EVENT HANDLERS ++++++++++++++++++++
         // NEXT Button click event handler
-        One.prototype._nextButtonClick = function (event) {
+        One.prototype._leftButtonClick = function (event) {
             // Switch to the OVER Scene
             scene = config.Scene.PLAY;
             changeScene();
         };
         // BACK Button click event handler
-        One.prototype._backButtonClick = function (event) {
+        One.prototype._rightButtonClick = function (event) {
             // Switch to the OVER Scene
             scene = config.Scene.MENU;
             changeScene();
