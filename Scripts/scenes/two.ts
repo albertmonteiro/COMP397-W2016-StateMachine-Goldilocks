8df1f20599a03b8ffe4c1a05cd6b5c61c760c9f0
albertmonteiro <albertmonteiro@live.com>
2016-02-03 02:12:00 -0500

    testing

--- Scripts/scenes/two.ts
+++ Scripts/scenes/two.ts
@@ -18,17 +18,13 @@ module scenes {
 
             // add the PLAY label to the scene
             this._playLabel = new createjs.Text("", "20px Consolas", "#000000");
-            // this._playLabel.text = "You continue \n\n";
-            // this._playLabel.text += "You have been \n\n";
             this._playLabel.text = "You continue your quest for the HOT CHOCOLATE. \n\n";
             this._playLabel.text += "You have been walking for a while now. \n\n";
-            // this._playLabel.text += "You are faced with yet another decision. \n\n";
-            // this._playLabel.text += "You have to choose from between two roads again. \n\n";
-            // this._playLabel.text += "Do you go LEFT or do you go RIGHT?";
+            this._playLabel.text += "You are faced with yet another decision. \n\n";
+            this._playLabel.text += "You have to choose from between two roads again. \n\n";
+            this._playLabel.text += "Do you go LEFT or do you go RIGHT?";
             // this._playLabel.regX = this._playLabel.getMeasuredWidth() * 0.5;
             // this._playLabel.regY = this._playLabel.getMeasuredHeight() * 0.5;
-            // this._playLabel.regX = this._playLabel.getMeasuredWidth();
-            // this._playLabel.regY = this._playLabel.getMeasuredHeight();
             // this._playLabel.x = config.Screen.CENTER_X;
             // this._playLabel.y = config.Screen.CENTER_Y;
             this.addChild(this._playLabel);
