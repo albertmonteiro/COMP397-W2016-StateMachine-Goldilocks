c2f403a5457278f84dc945e5277a031aa5ce9c57
albertmonteiro <albertmonteiro@live.com>
2016-02-03 01:38:06 -0500

    testing

--- Scripts/scenes/two.ts
+++ Scripts/scenes/two.ts
@@ -20,13 +20,13 @@ module scenes {
             this._playLabel = new createjs.Text("", "14px Consolas", "#000000");
             this._playLabel.text = "You continue your quest for the HOT CHOCOLATE. \n\n";
             this._playLabel.text += "You have been walking for a while now. \n\n";
-            this._playLabel.text += "You are faced with yet another decision. \n\n";
-            this._playLabel.text += "You have to choose from between two roads again. \n\n";
-            this._playLabel.text += "Do you go LEFT or do you go RIGHT?";
+            // this._playLabel.text += "You are faced with yet another decision. \n\n";
+            // this._playLabel.text += "You have to choose from between two roads again. \n\n";
+            // this._playLabel.text += "Do you go LEFT or do you go RIGHT?";
             this._playLabel.regX = this._playLabel.getMeasuredWidth() * 0.5;
             this._playLabel.regY = this._playLabel.getMeasuredHeight() * 0.5;
-            // this._playLabel.x = config.Screen.CENTER_X;
-            // this._playLabel.y = config.Screen.CENTER_Y;
+            this._playLabel.x = config.Screen.CENTER_X;
+            this._playLabel.y = config.Screen.CENTER_Y;
             this.addChild(this._playLabel);
 
             // add the LEFT button to the PLAY scene
