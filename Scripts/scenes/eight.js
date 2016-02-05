994ece7f1c1718104039fc82217ca2442705b0b5
albertmonteiro <albertmonteiro@live.com>
2016-02-05 02:21:22 -0500

    Testing

--- Scripts/scenes/eight.js
+++ Scripts/scenes/eight.js
@@ -16,8 +16,7 @@ var scenes;
         // Start Method
         Eight.prototype.start = function () {
             // add the PLAY label to the scene
-            this._playLabel = new createjs.Text("", "25px Consolas", "#000000");
-            // this._playLabel.text += "Welcome to GOLDILOCKS quest! \n\n";
+            this._playLabel = new createjs.Text("", "20px Consolas", "#000000");
             this._playLabel.text += "A big bear jumps out and  \n\n";
             this._playLabel.text += "eats you up! \n\n";
             this._playLabel.regX = this._playLabel.getMeasuredWidth() * 0.5;
@@ -25,11 +24,14 @@ var scenes;
             this._playLabel.x = config.Screen.CENTER_X;
             this._playLabel.y = config.Screen.CENTER_Y;
             this.addChild(this._playLabel);
-            // add the LEFT button to the PLAY scene
-            this._leftButton = new objects.Button("SadFace", config.Screen.CENTER_X - 100, config.Screen.CENTER_Y + 170);
-            this.addChild(this._leftButton);
-            // LEFT Button event listener
-            this._leftButton.on("click", this._leftButtonClick, this);
+            // // add the LEFT button to the PLAY scene
+            // this._leftButton = new objects.Button(
+            //     "SadFace",
+            //     config.Screen.CENTER_X - 100,
+            //     config.Screen.CENTER_Y + 170);
+            // this.addChild(this._leftButton);
+            // // LEFT Button event listener
+            // this._leftButton.on("click", this._leftButtonClick, this);
             // add the RIGHT button to the PLAY scene
             this._rightButton = new objects.Button("StartOverButton", config.Screen.CENTER_X + 100, config.Screen.CENTER_Y + 170);
             this.addChild(this._rightButton);
--- Scripts/scenes/eight.ts
+++ Scripts/scenes/eight.ts
@@ -17,8 +17,7 @@ module scenes {
         public start(): void {
 
             // add the PLAY label to the scene
-            this._playLabel = new createjs.Text("", "25px Consolas", "#000000");
-            // this._playLabel.text += "Welcome to GOLDILOCKS quest! \n\n";
+            this._playLabel = new createjs.Text("", "20px Consolas", "#000000");
             this._playLabel.text += "A big bear jumps out and  \n\n";
             this._playLabel.text += "eats you up! \n\n";
             this._playLabel.regX = this._playLabel.getMeasuredWidth() * 0.5;
@@ -27,14 +26,14 @@ module scenes {
             this._playLabel.y = config.Screen.CENTER_Y;
             this.addChild(this._playLabel);
 
-            // add the LEFT button to the PLAY scene
-            this._leftButton = new objects.Button(
-                "SadFace",
-                config.Screen.CENTER_X - 100,
-                config.Screen.CENTER_Y + 170);
-            this.addChild(this._leftButton);
-            // LEFT Button event listener
-            this._leftButton.on("click", this._leftButtonClick, this);
+            // // add the LEFT button to the PLAY scene
+            // this._leftButton = new objects.Button(
+            //     "SadFace",
+            //     config.Screen.CENTER_X - 100,
+            //     config.Screen.CENTER_Y + 170);
+            // this.addChild(this._leftButton);
+            // // LEFT Button event listener
+            // this._leftButton.on("click", this._leftButtonClick, this);
 
             // add the RIGHT button to the PLAY scene
             this._rightButton = new objects.Button(
